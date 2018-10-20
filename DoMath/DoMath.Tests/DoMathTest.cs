c9b5a7df77a0b6430b1b24599e9e09c51d0412c8
bzalewa <beata.zalewa@gmail.com>
2018-10-20 22:38:25 +0200

    Final Commit
    Test project with NUnit library.

---
+++ DoMath/DoMath.Tests/DoMathTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+#region Version 1
+/*namespace DoMath.Tests
+{
+    [TestFixture]
+    public class DoMathTest
+    {
+            [Test]
+            public void ShouldAdd2Numbers()
+            {
+                IDoMath idm = new Math();   
+                int expectedResult = idm.Add(7, 8); 
+                Assert.That(expectedResult, Is.EqualTo(15));  
+        }
+
+            [Test]
+            public void ShouldMultiply2Numbers()
+            {
+                IDoMath idm = new Math();
+                int expectedResult = idm.Multiply(7, 8);
+                Assert.That(expectedResult, Is.EqualTo(56));
+            }
+    }
+}
+*/
+#endregion
+#region Verion 2
+
+namespace DoMath.Tests
+{
+    [TestFixture]
+    public class DoMathTest
+    {
+        IDoMath idm;
+        [SetUp]
+        public void TestSetUp()
+        {
+            idm = new Math();
+        }
+        [Test]
+        public void ShouldAdd2Numbers()
+        {
+
+            int expectedResult = idm.Add(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(15));
+        }
+        [Test]
+        [Ignore("Test ignored until Monday")]
+        public void ShouldMultiply2Numbers()
+        {
+
+            int expectedResult = idm.Multiply(7, 8);
+            Assert.That(expectedResult, Is.EqualTo(56));
+        }
+        [TearDown]
+        public void TestTearDown()
+        {
+            idm = null;
+        }
+    }
+}
+#endregion
---
+++ DoMath/DoMath/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoMath
+{
+    public class Math : IDoMath
+    {
+        public int Add(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            int result = number1 + number2;
+            return result;
+        }
+
+    }
+}
